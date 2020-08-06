3d6d8f48ccbb7163cb780bf88026c14621479e9a
kleberson84 <kleberson84@users.noreply.github.com>
2020-08-06 18:52:43 +0200

    Added converter component

---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styles: [
+  ]
+})
+export class ConverterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

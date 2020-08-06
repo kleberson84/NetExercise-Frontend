dd7a5b926239d21e65a1e8b8e6ea3c932b548c42
kleberson84 <kleberson84@users.noreply.github.com>
2020-08-06 19:07:54 +0200

    Added model, form and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ConverterComponent } from './converter/converter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConverterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ConverterComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TextWebModel } from 'src/app/shared/text-web-model.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-converter',
-  templateUrl: './converter.component.html',
-  styles: [
-  ]
+	selector: 'app-converter',
+	templateUrl: './converter.component.html',
+	styles: [
+	]
 })
 export class ConverterComponent implements OnInit {
+	inputText: TextWebModel;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.resetForm();
+	}
 
+	resetForm(form?: NgForm) {
+		if (form != null) {
+			form.resetForm();
+		}
+		this.inputText = { content: '' }
+	}
 }
